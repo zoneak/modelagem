18807e07b92e81a68a1e0b59731d8fcda1b49e3a
Adriano Kaito <adriano.kaito@gmail.com>
2021-08-19 17:37:57 -0300

    primeiro commit - teste REST

---
+++ src/main/java/com/ak/modelagem/ModelagemApplication.java
@@ -0,0 +1,13 @@
+package com.ak.modelagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ak/modelagem/ModelagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ak.modelagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
