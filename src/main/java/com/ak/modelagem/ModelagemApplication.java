d758ffefa438808058e7b6af012823af1299c0c6
Adriano Kaito <adriano.kaito@gmail.com>
2021-08-20 12:15:18 -0300

    criando operacao de instanciacao

--- src/main/java/com/ak/modelagem/ModelagemApplication.java
+++ src/main/java/com/ak/modelagem/ModelagemApplication.java
@@ -1,13 +1,31 @@
 package com.ak.modelagem;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ModelagemApplication {
+import com.ak.modelagem.domain.Categoria;
+import com.ak.modelagem.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ModelagemApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
