a3d5e98d2767834a73073d785c8e007bb9d85b8d
Adriano Kaito <adriano.kaito@gmail.com>
2021-08-20 11:52:45 -0300

    adicionado h2 e criada tabela Categoria

--- src/main/java/com/ak/modelagem/domain/Categoria.java
+++ src/main/java/com/ak/modelagem/domain/Categoria.java
@@ -2,11 +2,22 @@ package com.ak.modelagem.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 
 	public Categoria() {
