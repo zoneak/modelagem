a87ffeb77a4c96ec76fc675c23f1c2854dc48d12
Adriano Kaito <adriano.kaito@gmail.com>
2021-08-23 15:52:47 -0300

    criacao de paginacao na lista de categorias

--- src/main/java/com/ak/modelagem/resources/CategoriaResource.java
+++ src/main/java/com/ak/modelagem/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -30,6 +32,18 @@ public class CategoriaResource {
 		List<CategoriaDTO> listaDTO = lista.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList()); // Transforma em lista de DTO
 		return ResponseEntity.ok().body(listaDTO);	
 	}
+	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy) {
+		Page<Categoria> lista = categoriaService.findPage(page, linesPerPage, direction, orderBy);
+		Page<CategoriaDTO> listaDTO = lista.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(listaDTO);	
+	}
+	
 	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Long id) {
--- src/main/java/com/ak/modelagem/services/CategoriaService.java
+++ src/main/java/com/ak/modelagem/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ak.modelagem.domain.Categoria;
@@ -44,6 +47,11 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityCustomException("Não é possível excluir uma categoria que possui produtos associados");
 		}
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String direction, String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
 	}
 
 }
