9001189f024a1d99df88ebffa07ebcb5b7491513
Adriano Kaito <adriano.kaito@gmail.com>
2021-08-25 11:42:40 -0300

    subtotal e total do valor do pedido

--- src/main/java/com/ak/modelagem/domain/Pedido.java
+++ src/main/java/com/ak/modelagem/domain/Pedido.java
@@ -52,6 +52,14 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido i : itens) {
+			soma += i.getSubTotal();
+		}
+		return soma;
+	}
 
 	public Long getId() {
 		return id;
