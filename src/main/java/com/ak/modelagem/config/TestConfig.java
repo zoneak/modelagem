32ab62f6ea00e1c9d594bdce3d9a5d2097dd1b80
Adriano Kaito <adriano.kaito@gmail.com>
2021-08-26 17:07:40 -0300

    configuracao inicial do spring security

--- src/main/java/com/ak/modelagem/config/TestConfig.java
+++ src/main/java/com/ak/modelagem/config/TestConfig.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.ak.modelagem.services.DBService;
 import com.ak.modelagem.services.EmailService;
@@ -28,4 +30,9 @@ public class TestConfig {
 	public EmailService emailService() {
 		return new MockEmailService();
 	}
+	
+	 @Bean
+    public JavaMailSender javaMailSender() { 
+          return new JavaMailSenderImpl();
+    }
 }
