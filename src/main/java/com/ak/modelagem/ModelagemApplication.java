554b8f07355bf51e90ef8e08a502b8a011971cd3
Adriano Kaito <adriano.kaito@gmail.com>
2021-08-30 15:00:48 -0300

    salvando arquivo no amazon s3

--- src/main/java/com/ak/modelagem/ModelagemApplication.java
+++ src/main/java/com/ak/modelagem/ModelagemApplication.java
@@ -1,11 +1,17 @@
 package com.ak.modelagem;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ak.modelagem.services.S3Service;
+
 @SpringBootApplication
 public class ModelagemApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemApplication.class, args);
@@ -13,7 +19,7 @@ public class ModelagemApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("C:\\Users\\ak_zo\\Pictures\\ak.jpg");
 	}
 
 }
