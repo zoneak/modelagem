5d3c1724c8c91ec8ba26878fae88ec8f61013deb
Adriano Kaito <adriano.kaito@gmail.com>
2021-08-31 11:57:42 -0300

    expondo o header Authorization

--- src/main/java/com/ak/modelagem/security/JWTAuthenticationFilter.java
+++ src/main/java/com/ak/modelagem/security/JWTAuthenticationFilter.java
@@ -55,6 +55,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String username = ((UserSpringSecurity) authResult.getPrincipal()).getUsername();
         String token = jwtUtil.generateToken(username);
         response.addHeader("Authorization", "Bearer " + token);
+        response.addHeader("access-control-expose-headers", "Authorization");
 	}
 	
 	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
